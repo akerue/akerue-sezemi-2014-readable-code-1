82c817fadac6cbbfa9426c40d80f3a61825f0637
akerue <hsmgn8118akerue@gmail.com>
2014-06-22 11:28:19 +0900

    Add function to input from text file

--- recipe.py
+++ recipe.py
@@ -1,3 +1,12 @@
 #_*_coding:utf-8_*_
 
-print "オムライス"
+import codecs
+
+with codecs.open("./data/recipe-data.txt", "r", encoding="utf-8") as f:
+    # 改行区切りのレシピを取得
+    recipes = f.readlines()
+
+# 今後レシピの数が増える可能性があるため，イテレートしている
+for recipe in recipes:
+    # 空行が表示されるのを防ぐ
+    print recipe.rstrip()
